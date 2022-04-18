03dfbf3bd53699b4824965b5d931d0e52bdd7623
JeykerSalinas <jeyker.salinas13@gmail.com>
2022-04-17 22:54:56 -0400

    Job done

---
+++ src/components/TaskList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item my-2" v-for="(item, i) in items" :key="i">
+        {{ item }}
+        <button class="btn btn-danger btn-sm" @click="removeTask(i)">
+          Borrar
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TaskList",
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods: {
+    removeTask(index) {
+      this.$emit("click", index);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
